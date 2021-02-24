652fbd834438ea6fa081cf1650250431381257b4
Martin Sucha <git@mm.ms47.eu>
2021-02-24 08:43:28 +0100

    Change the file format
    Added magic to determine file type / version and moved writing of
    JSON to the end so that it can include any trailers or information
    about how long the data was downloading, etc.

--- repository/repository.go
+++ repository/repository.go
@@ -1,6 +1,22 @@
+// Package repository implements storing HTTP responses in filesystem.
+//
+// Files are stored in a directory with the cache key in filename encoded using base32.
+// Base32 is used so that the encoding will work on case insensitive filesystems.
+//
+// File format of individual files is as follows:
+//
+//  Field        Type             Description
+//  magic        [4]byte          "STS1" identifying the file format
+//  body_size    uint64_le        length of body data in bytes
+//  body_sha256  [32]byte         SHA-256 digest of body data
+//  json_size    uint32_le        length of JSON data in bytes
+//  json_crc32   uint32_le        IEEE crc32 checksum of JSON data
+//  body_data    [body_size]byte  Data of the body
+//  json_data    [json_size]byte  JSON data describing the request
 package repository
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/base32"
 	"encoding/binary"
@@ -21,7 +37,7 @@ type Repository struct {
 	path string
 }
 
-type DocumentHeader struct {
+type DocumentMetadata struct {
 	Key            string
 	DownloadedDate time.Time
 	URL            string
@@ -29,7 +45,9 @@ type DocumentHeader struct {
 }
 
 type Document struct {
-	Header     DocumentHeader
+	Metadata   DocumentMetadata
+	BodySHA256 [sha256.Size]byte
+	BodySize   int64
 	f          *os.File
 	bodyOffset int64
 }
@@ -38,9 +56,9 @@ func New(path string) *Repository {
 	return &Repository{path: path}
 }
 
-const binaryHeaderSize = 8 + sha256.Size
+const binaryHeaderSize = 52
 
-func (r *Repository) Store(h *DocumentHeader, data io.Reader) (outErr error) {
+func (r *Repository) Store(h *DocumentMetadata, bodyData io.Reader) (outErr error) {
 	filename := keyToFilename(h.Key)
 	f, err := ioutil.TempFile(r.path, "tmp-"+filename)
 	if err != nil {
@@ -59,35 +77,43 @@ func (r *Repository) Store(h *DocumentHeader, data io.Reader) (outErr error) {
 			_ = os.Remove(f.Name())
 		}
 	}()
-	headerData, err := json.Marshal(h)
+
+	_, err = f.Seek(binaryHeaderSize, 0)
+	if err != nil {
+		return err
+	}
+
+	bodyHasher := sha256.New()
+	bodyWrittenBytes, err := io.Copy(f, io.TeeReader(bodyData, bodyHasher))
+	if err != nil {
+		return err
+	}
+
+	jsonData, err := json.Marshal(h)
 	if err != nil {
 		return
 	}
-	if len(headerData) > math.MaxUint32 {
-		return fmt.Errorf("json header size overflow: %d bytes", len(headerData))
+	if len(jsonData) > math.MaxUint32 {
+		return fmt.Errorf("json data size overflow: %d bytes", len(jsonData))
 	}
 
-	var binaryHeader [binaryHeaderSize]byte
-	binary.LittleEndian.PutUint32(binaryHeader[0:4], uint32(len(headerData)))
-	binary.LittleEndian.PutUint32(binaryHeader[4:8], crc32.ChecksumIEEE(headerData))
-	_, err = f.Write(binaryHeader[:])
+	_, err = f.Write(jsonData)
 	if err != nil {
 		return err
 	}
-	_, err = f.Write(headerData)
-	if err != nil {
-		return err
-	}
-	hasher := sha256.New()
-	_, err = io.Copy(f, io.TeeReader(data, hasher))
-	if err != nil {
-		return err
-	}
+
 	_, err = f.Seek(0, 0)
 	if err != nil {
 		return err
 	}
-	hasher.Sum(binaryHeader[8:8:binaryHeaderSize])
+
+	var binaryHeader [binaryHeaderSize]byte
+	copy(binaryHeader[0:4], "STS1")
+	binary.LittleEndian.PutUint64(binaryHeader[4:12], uint64(bodyWrittenBytes))
+	bodyHasher.Sum(binaryHeader[12:12:44])
+	binary.LittleEndian.PutUint32(binaryHeader[44:48], uint32(len(jsonData)))
+	binary.LittleEndian.PutUint32(binaryHeader[48:52], crc32.ChecksumIEEE(jsonData))
+
 	_, err = f.Write(binaryHeader[:])
 	if err != nil {
 		return err
@@ -118,23 +144,36 @@ func (r *Repository) Load(key string) (outDoc *Document, outErr error) {
 	case err != nil:
 		return nil, err
 	}
-	jsonHeaderSize := binary.LittleEndian.Uint32(binaryHeader[0:4])
-	jsonData := make([]byte, jsonHeaderSize)
-	_, err = io.ReadFull(f, jsonData)
+	if !bytes.Equal(binaryHeader[0:4], []byte("STS1")) {
+		return nil, fmt.Errorf("incorrect magic")
+	}
+
+	doc := &Document{
+		f: f,
+	}
+	doc.BodySize = int64(binary.LittleEndian.Uint64(binaryHeader[4:12]))
+	copy(doc.BodySHA256[:], binaryHeader[12:44])
+
+	jsonDataSize := binary.LittleEndian.Uint32(binaryHeader[44:48])
+	jsonData := make([]byte, jsonDataSize)
+	jsonChecksum := crc32.NewIEEE()
+	_, err = io.ReadFull(io.TeeReader(f, jsonChecksum), jsonData)
 	switch {
 	case errors.Is(err, io.EOF):
 		return nil, io.ErrUnexpectedEOF
 	case err != nil:
 		return nil, err
 	}
-	doc := &Document{
-		f:          f,
-		bodyOffset: binaryHeaderSize + int64(jsonHeaderSize),
+	jsonExpectedChecksum := binary.LittleEndian.Uint32(binaryHeader[48:52])
+	if jsonChecksum.Sum32() != jsonExpectedChecksum {
+		return nil, fmt.Errorf("crc32 checksum of metadata json does not match")
 	}
-	err = json.Unmarshal(jsonData, &doc.Header)
+
+	err = json.Unmarshal(jsonData, &doc.Metadata)
 	if err != nil {
 		return nil, err
 	}
+
 	return doc, nil
 }
 
