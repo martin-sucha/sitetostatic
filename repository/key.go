martin-sucha/sitetostatic

commit d41023882e13bb2d703a3105e4bd09fd3ab4f163

Move url normalization to separate package
This removes code duplication.

repository/key.go
@@ -2,38 +2,19 @@ package repository
 
 import (
 	"net/url"
+	"site-to-static/urlnorm"
 	"sort"
 	"strings"
 )
 
 // Key returns a canonical storage key for the given URL.
+// Applies changes from urlnorm.Canonical and on top of that, we:
+//
+//  - reorder query parameters
+//  - remove tracking query parameters
+//  - ignore fragment
 func Key(someURL *url.URL) string {
-	// Per RFC3986, the canonical form of URL has:
-	//
-	//  - lowercase scheme
-	//  - lowercase host / address
-	//  - port omitted if default for scheme
-	//  - colon between host:port not specified if port is empty
-	//
-	// On top of that, we:
-	//
-	//  - reorder query parameters
-	//  - remove tracking query parameters
-	//  - ignore fragment
-	u := new(url.URL)
-	*u = *someURL
-	u.Scheme = strings.ToLower(u.Scheme)
-	u.Fragment = ""
-	u.RawFragment = ""
-	host := u.Hostname()
-	port := u.Port()
-	if (u.Scheme == "https" && port == "443") || (u.Scheme == "http" && port == "80") {
-		port = ""
-	}
-	u.Host = joinHostPort(strings.ToLower(host), port)
-	if u.IsAbs() && u.Host != "" && u.Path == "" {
-		u.Path = "/"
-	}
+	u := urlnorm.Canonical(someURL)
 
 	var parts []queryParam
 	for k, v := range u.Query() {
@@ -74,23 +55,6 @@ func (q queryParam) String() string {
 			sb.WriteString("=")
 		}
 		sb.WriteString(url.QueryEscape(val))
-	}
-	return sb.String()
-}
-
-func joinHostPort(host, port string) string {
-	var sb strings.Builder
-	// Assume IPv6 address if host contains colon.
-	if strings.Contains(host, ":") {
-		sb.WriteString("[")
-		sb.WriteString(host)
-		sb.WriteString("]")
-	} else {
-		sb.WriteString(host)
-	}
-	if port != "" {
-		sb.WriteString(":")
-		sb.WriteString(port)
 	}
 	return sb.String()
 }

main.go
@@ -7,6 +7,7 @@ import (
 	"site-to-static/httrack"
 	"site-to-static/repository"
 	"site-to-static/scraper"
+	"site-to-static/urlnorm"
 	"strings"
 
 	"github.com/urfave/cli/v2"
@@ -114,7 +115,7 @@ func doList(c *cli.Context) error {
 			if err != nil {
 				return err
 			}
-			_, err = fmt.Println(canonicalURL(parsedURL))
+			_, err = fmt.Println(urlnorm.Canonical(parsedURL).String())
 			return err
 		}
 	}
@@ -155,45 +156,4 @@ func doList(c *cli.Context) error {
 	}
 
 	return nil
-}
-
-// canonicalURL returns the canonical form of URL.
-func canonicalURL(someURL *url.URL) string {
-	// Per RFC3986, the canonical form of URL has:
-	//
-	//  - lowercase scheme
-	//  - lowercase host / address
-	//  - port omitted if default for scheme
-	//  - colon between host:port not specified if port is empty
-	u := new(url.URL)
-	*u = *someURL
-	u.Scheme = strings.ToLower(u.Scheme)
-	host := u.Hostname()
-	port := u.Port()
-	if (u.Scheme == "https" && port == "443") || (u.Scheme == "http" && port == "80") {
-		port = ""
-	}
-
-	u.Host = joinHostPort(strings.ToLower(host), port)
-	if u.IsAbs() && u.Host != "" && u.Path == "" {
-		u.Path = "/"
-	}
-	return u.String()
-}
-
-func joinHostPort(host, port string) string {
-	var sb strings.Builder
-	// Assume IPv6 address if host contains colon.
-	if strings.Contains(host, ":") {
-		sb.WriteString("[")
-		sb.WriteString(host)
-		sb.WriteString("]")
-	} else {
-		sb.WriteString(host)
-	}
-	if port != "" {
-		sb.WriteString(":")
-		sb.WriteString(port)
-	}
-	return sb.String()
 }

urlnorm/urlnorm.go
@@ -1,0 +1,50 @@
+package urlnorm
+
+import (
+	"net/url"
+	"strings"
+)
+
+// Canonical returns the canonical form of URL.
+//
+// Per RFC3986, the canonical form of URL has:
+//
+//  - lowercase scheme
+//  - lowercase host / address
+//  - port omitted if default for scheme
+//  - colon between host:port not specified if port is empty
+//
+// Canonical also ensures that path of absolute URLs always starts with /
+func Canonical(someURL *url.URL) *url.URL {
+	u := new(url.URL)
+	*u = *someURL
+	u.Scheme = strings.ToLower(u.Scheme)
+	host := u.Hostname()
+	port := u.Port()
+	if (u.Scheme == "https" && port == "443") || (u.Scheme == "http" && port == "80") {
+		port = ""
+	}
+
+	u.Host = joinHostPort(strings.ToLower(host), port)
+	if u.IsAbs() && u.Host != "" && u.Path == "" {
+		u.Path = "/"
+	}
+	return u
+}
+
+func joinHostPort(host, port string) string {
+	var sb strings.Builder
+	// Assume IPv6 address if host contains colon.
+	if strings.Contains(host, ":") {
+		sb.WriteString("[")
+		sb.WriteString(host)
+		sb.WriteString("]")
+	} else {
+		sb.WriteString(host)
+	}
+	if port != "" {
+		sb.WriteString(":")
+		sb.WriteString(port)
+	}
+	return sb.String()
+}

commit 2daaa5b136a32f69aaab8958478c596b8b612011

Fix URL fragment appearing in storage key
The fragment should not be used in key.
This is fixes a regression from
d41023882e13bb2d703a3105e4bd09fd3ab4f163.

repository/key.go
@@ -36,6 +36,8 @@ func Key(someURL *url.URL) string {
 		rawQuery.WriteString(part.String())
 	}
 	u.RawQuery = rawQuery.String()
+	u.Fragment = ""
+	u.RawFragment = ""
 	return u.String()
 }
 